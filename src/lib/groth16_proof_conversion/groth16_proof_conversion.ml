11750ec4edc7a886043bbd767e982c91e09b939f
mrmr1993 <mr_1993@hotmail.co.uk>
2025-11-21 11:15:00 +0000

    Convert preamble

--- src/lib/groth16_proof_conversion/groth16_proof_conversion.ml
+++ src/lib/groth16_proof_conversion/groth16_proof_conversion.ml
@@ -1,8 +1,32 @@
 open Pickles.Impls.Step
 
-module type Inputs = sig end
+module type Inputs = sig
+  module ATE_LOOP_COUNT : sig
+    val length : int
+  end
+
+  module VK : sig
+    val delta_lines : 'a
+
+    val gamma_lines : 'a
+  end
+
+  module LineParser : sig
+    val parse : int -> int -> 'a -> 'b
+  end
+end
 
 module Make (Inputs : Inputs) = struct
+  open Inputs
+
+  let begin_ = 1
+
+  let end_ = ATE_LOOP_COUNT.length - 55
+
+  let delta_lines = LineParser.parse begin_ end_ VK.delta_lines
+
+  let gamma_lines = LineParser.parse begin_ end_ VK.gamma_lines
+
   let tags, cache, proof, provers =
     Pickles.compile
       ~public_input:(Input_and_output (Typ.unit, Typ.unit))
