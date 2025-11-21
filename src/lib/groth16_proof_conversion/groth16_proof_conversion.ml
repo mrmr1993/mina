71f80a49e3f9f4658751b598bdc73fe6466c32e6
mrmr1993 <mr_1993@hotmail.co.uk>
2025-11-21 11:12:04 +0000

    Functorize

--- src/lib/groth16_proof_conversion/groth16_proof_conversion.ml
+++ src/lib/groth16_proof_conversion/groth16_proof_conversion.ml
@@ -1,9 +1,14 @@
 open Pickles.Impls.Step
 
-let tags, cache, proof, provers =
-  Pickles.compile
-    ~public_input:(Input_and_output (Typ.unit, Typ.unit))
-    ~auxiliary_typ:Typ.unit ~max_proofs_verified:(module Pickles_types.Nat.N0)
-    ~name:"groth16_conversion_0"
-    ~choices:(fun ~self:_ -> [])
-    ()
+module type Inputs = sig end
+
+module Make (Inputs : Inputs) = struct
+  let tags, cache, proof, provers =
+    Pickles.compile
+      ~public_input:(Input_and_output (Typ.unit, Typ.unit))
+      ~auxiliary_typ:Typ.unit
+      ~max_proofs_verified:(module Pickles_types.Nat.N0)
+      ~name:"groth16_conversion_0"
+      ~choices:(fun ~self:_ -> [])
+      ()
+end
