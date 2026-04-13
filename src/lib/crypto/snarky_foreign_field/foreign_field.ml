131ed7b7a15411b0fac48e57298d2342c5def7b5
mrmr1993 <mr_1993@hotmail.co.uk>
2026-04-13 19:30:37 +0100

    Update `single_add` to use prover_value pattern

--- src/lib/crypto/snarky_foreign_field/foreign_field.ml
+++ src/lib/crypto/snarky_foreign_field/foreign_field.ml
@@ -225,6 +225,10 @@ module Field3 = struct
 
   type t = Circuit.Field.t * Circuit.Field.t * Circuit.Field.t
 
+  type limb_tuple = Limb.t * Limb.t * Limb.t
+
+  let of_limb_tuple : limb_tuple -> t = Tuple3.map ~f:Limb.to_field
+
   let of_constant (x : Constant.t) : t =
     let l0, l1, l2 = Constant.split x in
     ( Circuit.Field.constant (bignum_to_field_const l0)
@@ -627,100 +631,98 @@ let sign_to_bigint = function
 
 (** Single foreign field addition/subtraction using ForeignFieldAdd gate. *)
 let single_add (x : Field3.t) (y : Field3.t) ~(sign : sign)
-    ~(f : Bignum_bigint.t) : Field3.t * Circuit.Field.t =
+    ~(f : Bignum_bigint.t) : Field3.limb_tuple * Circuit.Field.t =
   let f0, f1, f2 = Field3.Constant.split f in
-  let compute_add () =
-    let x0, x1, x2 = x in
-    let y0, y1, y2 = y in
-    let xv0 = field_const_to_bignum (Circuit.As_prover.read_var x0) in
-    let xv1 = field_const_to_bignum (Circuit.As_prover.read_var x1) in
-    let xv2 = field_const_to_bignum (Circuit.As_prover.read_var x2) in
-    let yv0 = field_const_to_bignum (Circuit.As_prover.read_var y0) in
-    let yv1 = field_const_to_bignum (Circuit.As_prover.read_var y1) in
-    let yv2 = field_const_to_bignum (Circuit.As_prover.read_var y2) in
-    let x_big = Field3.Constant.combine (xv0, xv1, xv2) in
-    let y_big = Field3.Constant.combine (yv0, yv1, yv2) in
-    let s = sign_to_bigint sign in
-    let r = Bignum_bigint.(x_big + (s * y_big)) in
-    let overflow =
-      if Bignum_bigint.(f = zero) then Bignum_bigint.zero
-      else if Bignum_bigint.(s = one) && Bignum_bigint.(r >= f) then
-        Bignum_bigint.one
-      else if Bignum_bigint.(s = neg one) && Bignum_bigint.(r < zero) then
-        Bignum_bigint.(neg one)
-      else Bignum_bigint.zero
-    in
-    let l2_mask = Bignum_bigint.(two_to_2limb - one) in
-    let x01 = Bignum_bigint.(xv0 + shift_left xv1 limb_bits) in
-    let y01 = Bignum_bigint.(yv0 + shift_left yv1 limb_bits) in
-    let f01 = Bignum_bigint.(f0 + shift_left f1 limb_bits) in
-    let r01 = Bignum_bigint.(x01 + (s * y01) - (overflow * f01)) in
-    let carry = Bignum_bigint.(shift_right r01 (Int.( * ) 2 limb_bits)) in
-    let r01_masked = Bignum_bigint.(r01 land l2_mask) in
-    let r0_val = Bignum_bigint.(r01_masked land limb_mask) in
-    let r1_val = Bignum_bigint.(shift_right r01_masked limb_bits) in
-    let r2_val = Bignum_bigint.(xv2 + (s * yv2) - (overflow * f2) + carry) in
-    (r0_val, r1_val, r2_val, overflow, carry)
-  in
-  let cache = ref None in
-  let get_cached () =
-    match !cache with
-    | Some v ->
-        v
-    | None ->
-        let v = compute_add () in
-        cache := Some v ;
-        v
+  let module T = struct
+    type bi = Bignum_bigint.t
+    type t = { r0 : bi; r1 : bi; r2 : bi; overflow : bi; carry : bi }
+
+    let typ : (_, t) Circuit.Typ.t = Circuit.Typ.prover_value ()
+  end in
+  let witness =
+    Circuit.exists T.typ ~compute:(fun () ->
+        let x0, x1, x2 = x in
+        let y0, y1, y2 = y in
+        let xv0 = field_const_to_bignum (Circuit.As_prover.read_var x0) in
+        let xv1 = field_const_to_bignum (Circuit.As_prover.read_var x1) in
+        let xv2 = field_const_to_bignum (Circuit.As_prover.read_var x2) in
+        let yv0 = field_const_to_bignum (Circuit.As_prover.read_var y0) in
+        let yv1 = field_const_to_bignum (Circuit.As_prover.read_var y1) in
+        let yv2 = field_const_to_bignum (Circuit.As_prover.read_var y2) in
+        let x_big = Field3.Constant.combine (xv0, xv1, xv2) in
+        let y_big = Field3.Constant.combine (yv0, yv1, yv2) in
+        let s = sign_to_bigint sign in
+        let r = Bignum_bigint.(x_big + (s * y_big)) in
+        let overflow =
+          if Bignum_bigint.(f = zero) then Bignum_bigint.zero
+          else if Bignum_bigint.(s = one) && Bignum_bigint.(r >= f) then
+            Bignum_bigint.one
+          else if Bignum_bigint.(s = neg one) && Bignum_bigint.(r < zero) then
+            Bignum_bigint.(neg one)
+          else Bignum_bigint.zero
+        in
+        let l2_mask = Bignum_bigint.(two_to_2limb - one) in
+        let x01 = Bignum_bigint.(xv0 + shift_left xv1 limb_bits) in
+        let y01 = Bignum_bigint.(yv0 + shift_left yv1 limb_bits) in
+        let f01 = Bignum_bigint.(f0 + shift_left f1 limb_bits) in
+        let r01 = Bignum_bigint.(x01 + (s * y01) - (overflow * f01)) in
+        let carry = Bignum_bigint.(shift_right r01 (Int.( * ) 2 limb_bits)) in
+        let r01_masked = Bignum_bigint.(r01 land l2_mask) in
+        let r0_val = Bignum_bigint.(r01_masked land limb_mask) in
+        let r1_val = Bignum_bigint.(shift_right r01_masked limb_bits) in
+        let r2_val =
+          Bignum_bigint.(xv2 + (s * yv2) - (overflow * f2) + carry)
+        in
+        { T.r0 = r0_val; r1 = r1_val; r2 = r2_val; overflow; carry })
   in
   let r0 =
-    Circuit.exists Circuit.Field.typ ~compute:(fun () ->
-        let r0, _, _, _, _ = get_cached () in
-        bignum_to_field_const r0 )
+    Circuit.(
+      exists Limb.typ ~compute:(fun () ->
+          (As_prover.read T.typ witness).T.r0))
   in
   let r1 =
-    Circuit.exists Circuit.Field.typ ~compute:(fun () ->
-        let _, r1, _, _, _ = get_cached () in
-        bignum_to_field_const r1 )
+    Circuit.(
+      exists Limb.typ ~compute:(fun () ->
+          (As_prover.read T.typ witness).T.r1))
   in
   let r2 =
-    Circuit.exists Circuit.Field.typ ~compute:(fun () ->
-        let _, _, r2, _, _ = get_cached () in
-        bignum_to_field_const r2 )
+    Circuit.(
+      exists Limb.typ ~compute:(fun () ->
+          (As_prover.read T.typ witness).T.r2))
   in
   let overflow =
-    Circuit.exists Circuit.Field.typ ~compute:(fun () ->
-        let _, _, _, overflow, _ = get_cached () in
-        bignum_to_field_const overflow )
+    Circuit.(
+      exists Field.typ ~compute:(fun () ->
+          bignum_to_field_const (As_prover.read T.typ witness).T.overflow))
   in
   let carry =
-    Circuit.exists Circuit.Field.typ ~compute:(fun () ->
-        let _, _, _, _, carry = get_cached () in
-        bignum_to_field_const carry )
+    Circuit.(
+      exists Field.typ ~compute:(fun () ->
+          bignum_to_field_const (As_prover.read T.typ witness).T.carry))
   in
   let x0, x1, x2 = x in
   let y0, y1, y2 = y in
   let sign_const =
     match sign with
-    | Add ->
-        Circuit.Field.Constant.one
-    | Sub ->
-        Circuit.Field.Constant.(zero - one)
+    | Add -> Circuit.Field.Constant.one
+    | Sub -> Circuit.Field.Constant.(zero - one)
   in
   Circuit.assert_
     (ForeignFieldAdd
-       { left_input_lo = x0
-       ; left_input_mi = x1
-       ; left_input_hi = x2
-       ; right_input_lo = y0
-       ; right_input_mi = y1
-       ; right_input_hi = y2
-       ; field_overflow = overflow
-       ; carry
-       ; foreign_field_modulus0 = bignum_to_field_const f0
-       ; foreign_field_modulus1 = bignum_to_field_const f1
-       ; foreign_field_modulus2 = bignum_to_field_const f2
-       ; sign = sign_const
-       } ) ;
+       {
+         left_input_lo = x0;
+         left_input_mi = x1;
+         left_input_hi = x2;
+         right_input_lo = y0;
+         right_input_mi = y1;
+         right_input_hi = y2;
+         field_overflow = overflow;
+         carry;
+         foreign_field_modulus0 = bignum_to_field_const f0;
+         foreign_field_modulus1 = bignum_to_field_const f1;
+         foreign_field_modulus2 = bignum_to_field_const f2;
+         sign = sign_const;
+       });
   ((r0, r1, r2), overflow)
 
 (** Sum a list of Field3 values with given signs.
@@ -749,7 +751,7 @@ let sum (xs : Field3.t list) (signs : sign list) ~(f : Bignum_bigint.t) :
     let result = ref (List.hd_exn xs) in
     List.iter2_exn (List.tl_exn xs) signs ~f:(fun xi sign_i ->
         let r, _overflow = single_add !result xi ~sign:sign_i ~f in
-        result := r ) ;
+        result := Field3.of_limb_tuple r ) ;
     let r0, r1, r2 = !result in
     Circuit.assert_
       (Raw { kind = Zero; values = [| r0; r1; r2 |]; coeffs = [||] }) ;
@@ -1238,7 +1240,7 @@ module Sum = struct
       let result = ref (List.hd_exn xs) in
       List.iter2_exn (List.tl_exn xs) t.ops ~f:(fun xi sign_i ->
           let r, _overflow = single_add !result xi ~sign:sign_i ~f in
-          result := r ) ;
+          result := Field3.of_limb_tuple r ) ;
       let r0, r1, r2 = !result in
       Circuit.assert_
         (Raw { kind = Zero; values = [| r0; r1; r2 |]; coeffs = [||] }) ;
@@ -1379,10 +1381,10 @@ module Sum = struct
         List.iteri (List.tl_exn xs) ~f:(fun i xi ->
             let sign_i = List.nth_exn signs i in
             let r, overflow = single_add !result xi ~sign:sign_i ~f in
-            let r0, _, _ = r in
+            let r0, _, _ = Field3.of_limb_tuple r in
             Circuit.assert_ (Equal (r0, x0s.(i))) ;
             Circuit.assert_ (Equal (overflow, overflows.(i))) ;
-            result := r ) ;
+            result := Field3.of_limb_tuple r ) ;
         ( if not t.chained then
           let r0, r1, r2 = !result in
           Circuit.assert_
